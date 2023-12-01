4aca805d400e5dee10e2f659c9b6a410d4061f7d
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2023-12-01 15:33:07 +0900

    add files for docker compose

--- docker/build.sh
+++ docker/build.sh
@@ -4,3 +4,6 @@ BASE_IMG=irslrepo/irsl_system:noetic
 
 docker build .. --progress=plain --pull -f Dockerfile \
        --build-arg BASE_IMAGE=${BASE_IMG} -t irslrepo/irsl_universal_robot:noetic
+
+## docker login
+## docker push irslrepo/irsl_universal_robot:noetic
