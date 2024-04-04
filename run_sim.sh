724f43c53a05f61cf7fb393ee9e828ca55cbb19c
Yohei Kakiuchi <kakiuchi.yohei.sw@tut.jp>
2024-04-04 23:31:31 +0900

    fix run_sim.sh

--- run_sim.sh
+++ run_sim.sh
@@ -4,4 +4,9 @@
 
 source /irsl_entryrc
 
-roslaunch irsl_choreonoid_ros run_sim_with_setup_cnoid.launch choreonoid_arg:="--python $(rospack find ur5e_robot)/launch/setup_cnoid.py" control_namespace:=/ur5e model_namespace:=ur5e_ros model_file:="$(rospack find ur5e_robot)/model/ur5e.urdf" controllers:='joint_state_controller trajectory_controller'
+roslaunch irsl_choreonoid_ros run_sim_with_setup_cnoid.launch \
+          python_script:="--python $(rospack find ur5e_robot)/launch/setup_cnoid.py" \
+          control_namespace:=/ur5e \
+          model_namespace:=ur5e_ros \
+          model_file:="$(rospack find ur5e_robot)/model/ur5e.urdf" \
+          controllers:='joint_state_controller trajectory_controller gripper_controller'
